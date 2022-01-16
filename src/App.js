6f8aa334714bee97cc35d60d70c0f4204d77279e
Adrian Acedo <adrian@coderei.dev>
2022-01-16 08:59:59 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import PricesManager from './components/PricesManager';
 
 function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
+      <PricesManager />
+    </div >
+  )
 }
 
 export default App;
---
+++ src/components/PricesTable.js
@@ -0,0 +1,50 @@
+
+function PricesTable(props) {
+  const { elements, sortByCallback, removeResultCallback } = props
+
+  return (
+    <div className="mt-3 flex flex-col">
+      <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+        <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+          <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+            <table className="min-w-full divide-y divide-gray-200">
+              <thead className="bg-gray-50">
+                <tr>
+                  <th scope="col" className="cursor-pointer px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider" onClick={() => sortByCallback('item_id')}>
+                    Objeto
+                  </th>
+                  <th scope="col" className="cursor-pointer px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider" onClick={() => sortByCallback('city')}>
+                    Ciudad
+                  </th>
+                  <th scope="col" className="cursor-pointer px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider" onClick={() => sortByCallback('sell_price_min')}>
+                    Venta - Min
+                  </th>
+                  <th scope="col" className="cursor-pointer px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider" onClick={() => sortByCallback('buy_price_max')}>
+                    Compra - Max
+                  </th>
+                  <th scope="col" className="">
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                {elements?.map((e, index) => (
+                  <tr key={index} className={index % 2 === 0 ? 'bg-white' : 'bg-gray-50'}>
+                    <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">{e.item_id}</td>
+                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{e.city}</td>
+                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{e.sell_price_min}</td>
+                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{e.buy_price_max}</td>
+                    <td className="cursor-pointer px-6 py-4 whitespace-nowrap text-right text-sm font-medium" onClick={() => removeResultCallback(index)}>
+                      Eliminar
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default PricesTable
