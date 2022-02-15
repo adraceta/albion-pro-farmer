7d9edd8251fb77e4bf09d5abd574f741f15fed60
Adrian Acedo <adrian@coderei.dev>
2022-02-15 09:29:26 +0100

    feat: changed some classnames

--- src/components/PricesTable.js
+++ src/components/PricesTable.js
@@ -10,7 +10,7 @@ function PricesTable(props) {
             <table className="min-w-full divide-y divide-gray-200">
               <thead className="bg-gray-50">
                 <tr>
-                  <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                  <th scope="col" className="px-6 py-3 tracking-wider">
                   </th>
                   <th scope="col" className="cursor-pointer px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider" onClick={() => sortByCallback('item_id')}>
                     Objeto
