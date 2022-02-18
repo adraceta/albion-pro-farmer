e250e0708d1f94b89b14d164d38eb4f1468e8965
Adrian Acedo <adrian@coderei.dev>
2022-02-18 09:43:41 +0100

    feat: added GA

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import PricesManager from './components/PricesManager';
+import ReactGA from 'react-ga4';
 
 function App() {
-  
+
+  ReactGA.initialize('G-GGL2SMQLJY')
+  ReactGA.send("pageview")
+
   return (
     <div className="max-w-7xl mx-auto px-6 lg:px-8">
       <PricesManager />
